9d1eb0500b846703dccacfd18f7ea04e95c23b42
Bruce Li <isanoob1897@outlook.com>
2024-05-30 15:51:49 -0700

    add

--- countries.session.sql
+++ countries.session.sql
@@ -9,7 +9,7 @@ INSERT INTO countries(n, p) VALUES ("CA", 210);
 
 -- @block
 INSERT INTO countries(n, p) VALUES ("MX", 110);
-
+INSERT INTO countries(n, p) VALUES ("UK", 100);
 -- retreive data block
 -- @block
 SELECT * FROM countries;
